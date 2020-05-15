0534b08c96ed7af30cb54480fffbf29fa89b16b3
Patrick Nguyen <patrick.nguyen154@gmail.com>
2020-05-15 13:06:36 -0600

    created route for main page

--- src/root/Root.js
+++ src/root/Root.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import Main from '../MainPage/MainPage';
-// import { Route } from 'react-router-dom';
-// import PetList from '../PetList/PetList'
-
+import { Route } from 'react-router-dom';
+import PetList from '../PetList/PetList';
 
 function Root() {
   return (
     <div>
       <h1>Petful</h1>
-      <Main />
+      <Route exact path='/' component={Main} />
+      <Route path='/pets' component={PetList} />
     </div>
   );
 }
