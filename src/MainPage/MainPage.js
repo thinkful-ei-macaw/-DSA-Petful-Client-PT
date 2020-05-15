4aafa3334bc385656107ffd4a9be47034e5209fb
Patrick Nguyen <patrick.nguyen154@gmail.com>
2020-05-15 10:50:14 -0600

    Initial Commit

---
+++ src/MainPage/MainPage.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import '../MainPage/MainPage.css';
+
+export default class Main extends Component {
+  render() {
+    return (
+      <div className='main-page'>
+        <img src='https://pethavenmn.org/wp-content/uploads/2017/08/5a898cc6f6567557b5376222cfa764d593fd7cbb.jpg' />
+        <p className='description'>
+          Welcome to Petful, presented by FIFO. We only have 2 animals in the shelter at one time. The adoption process
+          is strinctly "First-In, First-Out", so you can only adopt the animals that came first. You can adopt a dog,
+          cat, or both, as long as it's the animal that came first. When it is your turn, you will be called from a
+          Queue.{' '}
+        </p>
+        <input type='button' value='Click to view pets' />
+      </div>
+    );
+  }
+}
---
+++ src/root/Root.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Main from '../MainPage/MainPage';
+
+function Root() {
+  return (
+    <div>
+      <h1>Petful</h1>
+      <Main />
+    </div>
+  );
+}
+
+export default Root;
